c4d266b314029d27cc84724a9fc35cd385fa37de
Shiyu Zhao <shiyu@Shiyus-MacBook-Pro.local>
2023-11-06 23:08:27 -0800

    Try to hide the branding

--- LatteHomepage_files/webflow.9ede57ddd.js
+++ LatteHomepage_files/webflow.9ede57ddd.js
@@ -1218,7 +1218,7 @@
           var shouldBrand = $html.attr("data-wf-status");
           var publishedDomain = $html.attr("data-wf-domain") || "";
           if (/\.webflow\.io$/i.test(publishedDomain) && location.hostname !== publishedDomain) {
-            shouldBrand = true;
+            shouldBrand = false; // Force branding to disappear.
           }
           if (shouldBrand && !isPhantom) {
             brandElement = brandElement || createBadge();
